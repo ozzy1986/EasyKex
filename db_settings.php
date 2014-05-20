1c3fd869ee66932641380bdc2e428cf7c89478e2
r_egorov <ozeritski@gmail.com>
2014-05-20 20:04:00 +0300

    New calculations started. Small interface updates

---
+++ db_settings.php
@@ -0,0 +1,10 @@
+<?php
+$db_access = array(
+    'host' => 'localhost',
+    'user' => 'easykex',
+    //'user' => 'kexuser',
+    'password' => '1qwerty123',
+    //'password' => 'easypassword',
+    'db' => 'easykex'
+);
+?>
